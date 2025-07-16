c307b1e9c6fc03d39da55b9e3b5a747d8b79b756
Alonso Morales Vargas <alonsotri234@gmail.com>
2025-07-16 14:20:13 -0600

    Comprobacion de erroor y finalizacion de la clase problematica

--- src/proyectoprogra/ColaConPrioridad.java
+++ src/proyectoprogra/ColaConPrioridad.java
@@ -1,13 +1,63 @@
 
 package proyectoprogra;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 
 
 /**
  *
- * @author david
+ * @author Alonso Y Feno 
  */
 public class ColaConPrioridad {
 
     //Comprobar si el error esta arreglado 
+    //Si se arreglo compañero xd 
+    private List<Cliente> fila = new ArrayList<>();
+
+    public void agregarCliente(Cliente c) {
+        fila.add(c);
+        fila.sort(Comparator.comparingInt(a -> prioridadValor(a.getPrioridad())));
+    }
+
+    private int prioridadValor(char p) {
+        return switch (p) {
+            case 'A' -> 1;
+            case 'B', 'C' -> 2;
+            case 'D' -> 3;
+            case 'E' -> 4;
+            case 'F', 'G' -> 5;
+            default -> 6;
+        };
+    }
+
+    public Cliente siguienteClienteExcepto(char letra) {
+        for (Cliente c : fila) {
+            if (c.getPrioridad() != letra) {
+                fila.remove(c);
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public Cliente siguienteClienteSolo(char letra) {
+        for (Cliente c : fila) {
+            if (c.getPrioridad() == letra) {
+                fila.remove(c);
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void eliminarClientesPorTolerancia() {
+        fila.removeIf(Cliente::sinAtencion);
+    }
+
+    public List<Cliente> getFila() {
+        return fila;
+    }
 }

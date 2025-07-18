51a140f74ddf13d3387d5db37af927837b4574a1
david <david@DAVID>
2025-07-18 12:07:26 -0600

    Finalizacion de la Clase ColaConPrioridad y bien Comentada

--- src/proyectoprogra/ColaConPrioridad.java
+++ src/proyectoprogra/ColaConPrioridad.java
@@ -9,20 +9,14 @@ import java.util.List;
 
 /**
  *
- * @author Alonso Y Feno 
+ * @author Alonso Y Jefferson
  */
-public class ColaConPrioridad {
 
     //Comprobar si el error esta arreglado 
-    //Si se arreglo compañero xd 
-    //Se procede a declarar una lista la cual se llama fila y los objetos que contendra son de tipo cliente 
+    //Si se arreglo compañero xd
+public class ColaConPrioridad {
+    //Se  declara una lista la cual se llama fila y los objetos que contendra son de tipo cliente 
     private List<Cliente> fila = new ArrayList<>();
-    //Se agregar un cliente a la cola 
-    //Esta ordena la lista mediante el metodo prioridad valor y convierte la prioridad en tipo char para facilitar la comparacion 
-    public void agregarCliente(Cliente c) {
-        fila.add(c);
-        fila.sort(Comparator.comparingInt(a -> prioridadValor(a.getPrioridad())));
-    }
     // El metodo prioridad valor convierte la letra en un numero 
     //El cambio de numeros sera para que la letra con menor valor numerico tenga mayor prioridad
     private int prioridadValor(char p) {
@@ -35,7 +29,7 @@ public class ColaConPrioridad {
             default -> 6;
         };
     }
-    //Este metodo se encarga de buscar y dovolver el primer cliente cuya prioridad no sea igual a la letra asignada 
+    //Este metodo de buscay  devuelve el primer cliente cuya prioridad no sea igual a la letra asignada 
     //Al devolverlo de la fila lo elimina, si no hay ninguno lo retornara como vacio(null)
     public Cliente siguienteClienteExcepto(char letra) {
         for (Cliente c : fila) {
@@ -57,12 +51,18 @@ public class ColaConPrioridad {
         }
         return null;
     }
-    //Aqui se eliminaran todos los clientes que cumplan con la condision sin atencion 
+    //Este metodo elimina todos los clientes que cumplan con la condicion de atencion
     public void eliminarClientesPorTolerancia() {
         fila.removeIf(Cliente::sinAtencion);
     }
-    //Acá devolveremos la lsita actual de clientes en la fila 
+    //Acá  se devuelve la lista  actual de clientes en la fila 
     public List<Cliente> getFila() {
         return fila;
+    }
+    //Se agrega un cliente a la cola 
+    //Esta ordena la lista mediante el metodo prioridad valor y convierte la prioridad en tipo char para facilitar la comparacion 
+     public void agregarCliente(Cliente c) {
+        fila.add(c);
+        fila.sort(Comparator.comparingInt(a -> prioridadValor(a.getPrioridad())));
     }
 }
